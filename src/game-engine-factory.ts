9956582bbf440fd755a71f3fc9f2a76b36cd053c
Alex Solis <git@alexsol.is>
2026-03-23 21:03:05 -0400

    Add fractal-engine to game engine factory

--- src/game-engine-factory.ts
+++ src/game-engine-factory.ts
@@ -1,16 +1,16 @@
-// import { FractalEngine } from '../engines/fractal-engine/server';
 import { GameSetting } from './game';
 import { GameEngine } from './game-engine';
 import { GravityEngine } from '../engines/gravity-engine/server';
 import { StandardEngine } from '../engines/standard-engine/server';
+import { FractalEngine } from '../engines/fractal-engine/server';
 
 export class GameEngineFactory {
   static getGameEngine(settings: GameSetting): GameEngine {
     switch (settings.engine) {
       case 'standard-engine':
         return new StandardEngine(settings);
-      // case 'fractal-engine':
-      //   return new FractalEngine(settings);
+      case 'fractal-engine':
+        return new FractalEngine(settings);
       case 'gravity-engine':
         return new GravityEngine(settings);
       default:
